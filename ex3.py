bf7fce099527971dd03b2c675459da221e2849a2
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2020-03-26 23:50:04 +0100

    fixup! Init

--- ex3.py
+++ ex3.py
@@ -1,29 +1,32 @@
-#*******
-#* Read input from STDIN
-#* Use: echo or print to output your result to STDOUT, use the /n constant at the end of each result line.
-#* Use: sys.stderr.write() to display debugging information to STDERR
-#* ***/
+# *******
+# * Read input from STDIN
+# * Use: echo or print to output your result to STDOUT, use the /n constant at the end of each result line.
+# * Use: sys.stderr.write() to display debugging information to STDERR
+# * ***/
 import sys
 
 lines = []
 for line in sys.stdin:
-	lines.append(line.rstrip('\n'))
-	
+    lines.append(line.rstrip('\n'))
+
 n = int(lines[0])
-	
+
+
 def hourToMin(hour):
     return (hour-8)*60+1
-    
+
+
 def dayToMin(day):
     return (day-1)*600
 
-parsed = []    
-for i in range(1,n+1):
+
+parsed = []
+for i in range(1, n+1):
     day = int(lines[i][0])
     start = dayToMin(day)+hourToMin(int(lines[i][2:4]))+int(lines[i][5:7])
     end = dayToMin(day)+hourToMin(int(lines[i][8:10]))+int(lines[i][11:13])
     parsed.append([start, end])
-    
+
 sortedList = sorted(parsed, key=lambda x: x[0])
 uniques = []
 
@@ -37,11 +40,11 @@ for i in range(1, n):
         uniques.append([current[0], current[1]])
         current = sortedList[i]
 
-res = 0
+res = 1
 for i in range(len(uniques)):
     if res+59 < uniques[i][0]:
         break
-    res = uniques[i][1] +1
+    res = uniques[i][1] + 1
 
 beg = res
 end = res + 59
@@ -54,11 +57,13 @@ minute = beg - 1 - (day*600) - (hour*60)
 houre = ((end - 1 - (day*600)) // 60)
 minutee = end - 1 - (day*600) - (houre*60)
 
+
 def countToString(hour):
     if hour < 10:
         return "0"+str(hour)
     else:
         return str(hour)
-    
 
-print(str(day+1)+" "+countToString(hour+8)+":"+countToString(minute)+"-"+countToString(houre+8)+":"+countToString(minutee))
+
+print(str(day+1)+" "+countToString(hour+8)+":"+countToString(minute) +
+      "-"+countToString(houre+8)+":"+countToString(minutee))
